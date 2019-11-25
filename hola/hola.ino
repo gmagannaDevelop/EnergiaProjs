e661edecfca371a447f7dc75d41b6c346d15fed6
gmagannaDevelop <gmaganna.biomed@gmail.com>
2019-11-25 11:51:08 -0600

    Added some bitshifting, which still does not work.

--- hola/hola.ino
+++ hola/hola.ino
@@ -10,9 +10,11 @@
 #define DATA_PIN    P1_3        // data for AS1106
 #define CLOCK_PIN   P1_5        // clock for AS1106
 #define LOAD_PIN    P1_4        // load CS for AS1106
+#define P1_UP       P1_6        // pin to signal up
+#define P1_DOWN     P1_7        // pin to signal down.
 
 // Proto: LedControl(int dataPin, int clkPin, int csPin, int numDevices=1);
-LedControl lc=LedControl(DATA_PIN, CLOCK_PIN, LOAD_PIN, 1);
+LedControl lc=LedControl(DATA_PIN, CLOCK_PIN, LOAD_PIN, 4);
 
 unsigned long delaytime=100;
 
@@ -23,6 +25,8 @@ void setup() {
   pinMode(DATA_PIN,OUTPUT);
   pinMode(CLOCK_PIN,OUTPUT);
   pinMode(LOAD_PIN,OUTPUT);
+  pinMode(P1_UP, INPUT);
+  pinMode(P1_DOWN, INPUT);
   digitalWrite(LOAD_PIN,HIGH);
   // BLink to test...
   digitalWrite(RED_LED, HIGH);
@@ -45,12 +49,21 @@ void loop() {
   lc.clearDisplay(0);
 
   int i;
-  for (i=0; i<4; i++)
-  { 
-    lc.setColumn(0, i, B00111000);
-    delay(1000);
-    lc.clearDisplay(0);
-    delay(1000);
-  }  
+  byte pos_y = B00111000;
+   
+  if (P1_UP == 0)
+  {
+    if (pos_y != B11100000){
+      pos_y <<= 1;
+    }
+  }
+  if (P1_DOWN == 0)
+  {
+    if (pos_y != B00000111){
+      pos_y >>= 1;
+    }
+  }
+    
+  lc.setColumn(0, 0, pos_y);
 
 }
